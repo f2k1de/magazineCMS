1986b9eaddbb283b81be43049ea0180014a82c6a
freddy2001 <freddy2001@tools.wmflabs.org>
2017-09-29 17:34:03 +0200

    whitespace fixes

--- config.example.php
+++ config.example.php
@@ -1,14 +1,14 @@
 <?php
 // Rename to config.php and fill with data
-    return array(
-        "host" => "www.example.com",
-	"name" => "NameOfTheProject",
-        "https" => "true",
-        "hashsecret" => "s0m€secre++ex+",
-        "version" => "alpha",
-	"databaseprefix" => "databaseprefix_",
-	"statisticsurl" => "https://stats.example.com/",
-	"statisticsuser" => "someuser",
-	"statisticspassword" => "somethingsecret",
-    );
+	return array(
+		"host" => "www.example.com",
+		"name" => "NameOfTheProject",
+		"https" => "true",
+		"hashsecret" => "s0m€secre++ex+",
+		"version" => "alpha",
+		"databaseprefix" => "databaseprefix_",
+		"statisticsurl" => "https://stats.example.com/",
+		"statisticsuser" => "someuser",
+		"statisticspassword" => "somethingsecret",
+	);
 ?>
